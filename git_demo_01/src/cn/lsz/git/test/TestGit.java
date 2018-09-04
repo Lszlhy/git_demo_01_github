eb5e608c9856389e7f14d90c8ce49541bd262538
lsz <1073104452@qq.com>
2018-09-04 16:32:33 +0800

    在dev分支上开发，并首次提交，2.0

--- git_demo_01/src/cn/lsz/git/test/TestGit.java
+++ git_demo_01/src/cn/lsz/git/test/TestGit.java
@@ -4,5 +4,10 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("this is my first project share to git");
+		say();
+	}
+	
+	public static void say() {
+		System.out.println("hello");
 	}
 }
