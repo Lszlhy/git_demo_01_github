a301ca158badfbc7806be61aff29e316921bdd5a
lsz <1073104452@qq.com>
2018-09-04 17:06:22 +0800

    从git中央仓库检出并提交，版本3.0

--- git_demo_01/src/cn/lsz/git/test/TestGit.java
+++ git_demo_01/src/cn/lsz/git/test/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("this is my first project share to git");
 		say();
+		System.out.println("i am clone");
 	}
 	
 	public static void say() {
